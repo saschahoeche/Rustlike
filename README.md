093711f126f3bb4cde24e7301f77d3c2fb7b9215
saschahoeche <sascha.hoeche@gmx.com>
2022-05-18 12:42:19 +0200

    bevy setup starting window

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Rustlike
+A Rust based Roguelike
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use bevy::prelude::*;
+
+pub const CLEAR: Color = Color::rgb(0.0, 0.0, 0.0);
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .insert_resource(ClearColor(CLEAR))
+        .insert_resource(WindowDescriptor{
+        width: 800.0,
+        height: 800.0,
+        title: "Rustlike".to_string(),
+        resizable: false,
+        ..Default::default()
+    })
+    .add_plugins(DefaultPlugins)
+        .run();
 }
