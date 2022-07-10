519bf9b6027a9b60520280daaaf1c753540845de
saschahoeche <sascha.hoeche@gmx.com>
2022-07-10 23:57:08 +0200

    further ecs stuff and multithreading

--- src/components.rs
+++ src/components.rs
@@ -1,7 +1,7 @@
 pub use crate::prelude::*;
 
 #[derive(Clone, Copy, Debug, PartialEq)]
-pub struct Render{
+pub struct Render {
     pub color: ColorPair,
     pub glyph: FontCharType,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,27 @@
 extern crate core;
 
 mod camera;
+mod components;
 mod map;
 mod map_builder;
-mod components;
 mod spawner;
+mod systems;
 
 mod prelude {
     pub use bracket_lib::prelude::*;
-    pub use legion::*;
-    pub use legion::world::SubWorld;
     pub use legion::systems::CommandBuffer;
+    pub use legion::world::SubWorld;
+    pub use legion::*;
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
     pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
     pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
     pub use crate::camera::*;
+    pub use crate::components::*;
     pub use crate::map::*;
     pub use crate::map_builder::*;
-    pub use crate::components::*;
     pub use crate::spawner::*;
+    pub use crate::systems::*;
 }
 
 use prelude::*;
@@ -53,8 +55,9 @@ impl GameState for State {
         ctx.cls();
         ctx.set_active_console(1);
         ctx.cls();
-        // TODO: Execute Systems
-        // TODO: Render Draw Buffer
+        self.resources.insert(ctx.key);
+        self.systems.execute(&mut self.ecs, &mut self.resources);
+        render_draw_buffer(ctx).expect("Render Error");
     }
 }
 
